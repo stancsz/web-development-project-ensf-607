2e7f0ffaf7d18924c7bb555959641f0fa5dbd7f8
Ziad <zchemali@ucalgary.ca>
2020-12-14 18:56:54 -0700

    Created empty template for the Front-End

---
+++ project/course_oultine_app/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import FunInfo from './FunInfo.js'
+import FunGrade from './FunGrade.js'
+import FunOutcome from './FunOutcome.js'
+
+function App() {
+  return (
+    <div className="App">
+      
+    <div className="Info">FunInfo</div>
+    <div className="Outcome">FunOutcome</div>
+    <div className="FunGrade">FunGrade</div>
+   
+
+    </div>
+  );
+}
+
+export default App;
