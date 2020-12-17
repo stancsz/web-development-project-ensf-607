f40c74ef3cc4f27c580174221c5798d61dab672e
Ziad <zchemali@ucalgary.ca>
2020-12-16 19:14:50 -0700

    Added data folder, and added lotfi's FunInfo code

--- project/course_oultine_app/src/add_outline_componenets/FunInfo.js
+++ project/course_oultine_app/src/add_outline_componenets/FunInfo.js
@@ -1,11 +1,86 @@
-import React from 'react';
-const FunInfo=()=> {
-    return (
-      <div className="App">
-        <h1> This is Info section</h1>
-      </div>
-    );
-  }
+import "bulma/css/bulma.css";
+
+import { useState } from 'react';
+
+function FunInfo() {
+
+
+    const [courseNumberInput, setCourseNumberInput] = useState();
+    const [courseNameInput, setCourseNameInput] = useState();
+
+
+    var courseInfoPlaceholderText = "Enter Course Information Here. Example: A survey of software design"
+    + " and development topics for Engineering students. Topics include: key features of an object-oriented"
+    + " programming language, especially inheritance and polymorphism; elements of object-oriented design;"
+    + " programming and application of common data structures; strategies and tools for testing and debugging.";
+
   
-  export default FunInfo;
-  
+
+  return (
+    <div className="FunInfo">
+
+    <br/>
+      
+
+    <div className="field">
+
+        <br/>
+        
+        <label className="label is-size-3 has-text-left pl-1">1. Calendar Information</label>
+
+        <div className="control">
+            <input className="input is-size-4 has-text-left pl-1 mb-0 pb-0" 
+            value={courseNumberInput} onInput={e => setCourseNumberInput(e.target.value)} 
+            placeholder = "Enter Course Number. Example: ENSF 409"/>
+        </div>
+      
+        <div className="control">
+          <input className="input is-size-4 has-text-left pl-2 mt-0 pt-0" 
+          value={courseNameInput} onInput={e => setCourseNameInput(e.target.value)} 
+          placeholder = "Enter Course Name. Example: Principles of Software Development"></input>
+        </div>
+    
+        <div className = "control">
+            <textarea className="textarea is-info" placeholder = {courseInfoPlaceholderText} rows="4">
+            </textarea>
+        </div>
+
+        <div className="columns">
+        <div className="column is-one-quarter has-text-left pl-4 pt-4">Course Hours:</div>
+        <div className="column has-text-left">
+            <div className="control">
+                <input className="input" type="text" placeholder="Example: 3 units; H (3-2)"/>
+            </div>
+        </div>
+        </div>
+        
+        <div className="columns">
+        <div className="column is-one-quarter has-text-left pl-4 pt-4">Academic Credit:</div>
+        <div className="column has-text-left">
+            <div className="control">
+                <input className="input" type="text" placeholder="Example: 3"/>
+            </div>
+        </div>
+        </div>
+
+        <div className="columns">
+        <div className="column is-one-quarter has-text-left pl-4 pt-4">Calendar Reference:</div>
+        <div className="column has-text-left">
+        
+            <div className="control">
+                <input className="input" type="text" 
+                placeholder="Example: http://www.ucalgary.ca/pubs/calendar/current/software-engineering-for-engineers.html#38252"/>
+            </div>
+
+
+        </div>
+        </div>
+
+    </div>
+
+    </div>
+
+  );
+}
+
+export default FunInfo;
--- project/course_oultine_app/src/add_outline_componenets/FunOutcome.js
+++ project/course_oultine_app/src/add_outline_componenets/FunOutcome.js
@@ -1,9 +1,91 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 const FunOutcome=()=> {
+ 
+  const[ rows,setRows]=useState([{num:1,text:"Enter outcome"}])
+
+  const [count,setCount]=useState(1)
+  const addRow=()=>{
+    var x=rows[rows.length-1].num
+    let temp={num:x,text:"Enter outcome"}
+    rows.push({})
+    rows.sort()
+    setRows(rows)
+  }
+  const update=(num,txt)=>{
+    let newRows=rows.filter((row)=> row.num !== num)
+    let temp={num:num , text:txt}
+    newRows.push(temp)
+    newRows.sort()
+    var x=newRows[rows.length-1].num
+    let temp2={num:x+1,text:"Enter outcome"}
+    newRows.push(temp2)
+    setRows(newRows)
+   
+    
+
+  }
+  
     return (
-      <div className="App">
-        <h1> This is Outcome section</h1>
-      </div>
+      <div className="column">
+   
+      
+   <div className="table-container">
+        <table className="table">
+        <thead>
+        <tr>
+    <th>Number</th>
+    <th>Outcome</th>
+
+        </tr>
+
+        </thead>
+        <tbody>
+          {
+            rows.map((row)=>{
+              return (
+                <tr>
+                <td id={row.num}>
+                  {count}
+                </td>
+                <td>
+                  <input type="text" placeholder={row.text} className={row.num} 
+                   ></input>
+                   
+                </td>
+                
+              </tr>
+              
+    
+              )
+            })
+
+           
+          }
+         
+         
+          
+          
+           
+            
+          
+          
+          
+        </tbody>
+        
+        </table>
+        <button type="button" onClick={()=>{
+          setCount(count+1)
+          update(document.getElementById(count.toString),document.getElementsByClassName(count.toString))
+         
+          
+          console.log(rows)}}></button>
+        </div>
+        
+   
+      
+      
+    </div>
     );
   }
   
