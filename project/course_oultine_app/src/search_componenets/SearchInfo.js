0ba31e171813d20e665b4795c8ce98235d9d2c60
Ziad <zchemali@ucalgary.ca>
2020-12-17 20:50:13 -0700

    Working on Search main panel
    Its done, need to work on displaying the info from selected list

--- project/course_oultine_app/src/search_componenets/SearchInfo.js
+++ project/course_oultine_app/src/search_componenets/SearchInfo.js
@@ -1,20 +1,27 @@
 import React, { useState } from 'react';
 import InfoData from '../data/DataInfo.json'
-const FunInfo=()=> {
-  const info = InfoData["ENSF 409"].courseName
-  const courseList=Object.keys(InfoData)
+const FunInfo=(props)=> {
+  console.log(props)
+  //const info = InfoData["ENSF 409"].courseName
+  //const courseList=Object.keys(InfoData)
   //const node2=InfoData.Info.find((n)=> n['ENSF 607'])
   const [selectedCourse,setSelection]=useState()
   //const outcome
  // const grade
+//  "courseNum" : "ENSF 409",
+//  "courseName" : "Principles of Software Development", 
+//  "courseInfo" : "A survey of software design and development topics for Engineering students. Topics include: key features of an object-oriented programming language, especially inheritance and polymorphism; elements of object-oriented design; programming and application of common data structures; strategies and tools for testing and debugging.",
+//  "courseHours" : "3 units; H (3-2)",
+//  "academicCredit" : 3,
+//  "calendarReference
+  
     return (
       <div className="App">
-        {courseList.map((n)=>{
-          return n
-        })}
-       // {/* {console.log(courseList.map((course)=>{course}))} */}
         
-        <h1> This is Info section</h1>
+        {/* {console.log(courseList.map((course)=>{course}))} */}
+        
+        <h1> {props.info.calendarReference} </h1>
+        <h1>{props.info.courseNum} </h1>
        
       </div>
     );
