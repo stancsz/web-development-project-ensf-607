6b0f0e8bd03652acdc9c9b96b0d857a352bf3926
Stan Chen <stanleychen587@gmail.com>
2021-01-04 22:28:00 -0700

    finished serializers for coordinator

--- CourseOutlineBackend/courseoutline/serializers.py
+++ CourseOutlineBackend/courseoutline/serializers.py
@@ -4,17 +4,32 @@ from .models import Coordinator
 
 
 class CoordinatorSerializer(serializers.ModelSerializer):
-  CourseID = serializers.CharField(max_length=5000, required=True)
+  CourseID = serializers.CharField(max_length=100, required=True)
+  FName =  serializers.CharField(max_length=100, required=False)
+  LName =  serializers.CharField(max_length=100, required=False)
+  Phone =  serializers.CharField(max_length=100, required=False)
+  Office =  serializers.CharField(max_length=100, required=False)
+  Email =  serializers.CharField(max_length=100, required=False)
 
   def create(self, validated_data):
     # Once the request data has been validated, we can create a todo item instance in the database
     return Coordinator.objects.create(
-      CourseID=validated_data.get('CourseID')
+      CourseID=validated_data.get('CourseID'),
+      FName=validated_data.get('FName'),
+      LName=validated_data.get('LName'),
+      Phone=validated_data.get('Phone'),
+      Office=validated_data.get('Office'),
+      Email=validated_data.get('Email')
     )
 
   def update(self, instance, validated_data):
      # Once the request data has been validated, we can update the todo item instance in the database
     instance.CourseID = validated_data.get('CourseID', instance.CourseID)
+    instance.FName = validated_data.get('FName', instance.FName)
+    instance.LName = validated_data.get('LName', instance.LName)
+    instance.Phone = validated_data.get('Phone', instance.Phone)
+    instance.Office = validated_data.get('Office', instance.Office)
+    instance.Email = validated_data.get('Email', instance.Email)
     instance.save()
     return instance
 
