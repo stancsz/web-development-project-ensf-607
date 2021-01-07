10510a01d729971cf01fa5279057146c85bcaf24
Lotfi Hasni <64884166+hasnil@users.noreply.github.com>
2021-01-07 15:53:47 -0700

    need to fix scroll up

--- project/course_oultine_app/src/pages/SearchCourse.js
+++ project/course_oultine_app/src/pages/SearchCourse.js
@@ -48,6 +48,15 @@ import axios from "axios";
 
 
 
+//
+import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
+import Zoom from '@material-ui/core/Zoom';
+import PropTypes from 'prop-types';
+//
+
+
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -64,12 +73,68 @@ const useStyles = makeStyles((theme) => ({
   selectEmpty: {
     marginTop: theme.spacing(2),
   },
+  
+//
+  scrollUp: {
+    position: 'fixed',
+    bottom: theme.spacing(2),
+    right: theme.spacing(2),
+  },
+//
+
 }));
 
 
+//
+function ScrollTop(props) {
+  const { children, window } = props;
+  const classes = useStyles();
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+    disableHysteresis: true,
+    threshold: 100,
+  });
+
+  const handleClick = (event) => {
+    const anchor = (event.target.ownerDocument || document).querySelector('#back-to-top-anchor');
+
+    if (anchor) {
+      anchor.scrollIntoView({ behavior: 'smooth', block: 'center' });
+    }
+
+  
+  };
+
+  return (
+    <Zoom in={trigger}>
+      <div onClick={handleClick} role="presentation" className={classes.scrollUp}>
+        {children}
+      </div>
+    </Zoom>
+  );
+}
+
+//
 
 
-const SearchCourse = () => {
+//
+ScrollTop.propTypes = {
+  children: PropTypes.element.isRequired,
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window: PropTypes.func,
+};
+//
+
+
+
+
+const SearchCourse = (props) => {
 
   const courseList = Object.keys(InfoData);
   const classes = useStyles();
@@ -550,6 +615,15 @@ const SearchCourse = () => {
       <br/>
 
       {frame}
+
+
+      {/**/}
+      <ScrollTop {...props}>
+        <Fab color="secondary" size="small" aria-label="scroll back to top">
+          <KeyboardArrowUpIcon />
+        </Fab>
+      </ScrollTop>
+
 
     </>
   );
